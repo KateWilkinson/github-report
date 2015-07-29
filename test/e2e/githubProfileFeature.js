60646e0b27e93db4fe82f9f8d5223357bb54022f
KateWilkinson <kate.wilkinson@hotmail.com>
2015-07-29 17:45:11 +0100

    added more protractor tests

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,11 +1,42 @@
+// describe('GitHub profile finder', function() {
+//   it('finds profiles', function() {
+//     browser.get('http://localhost:8080');
+
+//     element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
+//     element(by.className('btn')).click();
+
+//     expect(element(by.binding('user.login')).getText()).
+//         toEqual('spike01');
+//   });
+// });
+
 describe('GitHub profile finder', function() {
-  it('finds profiles', function() {
+
+  var searchBox = element(by.model('searchCtrl.searchTerm'))
+  var searchButton = element(by.className('btn'))
+
+  beforeEach(function() {
     browser.get('http://localhost:8080');
+  })
 
-    element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
-    element(by.className('btn')).click();
-
-    expect(element(by.binding('user.login')).getText()).
-        toEqual('spike01');
+  it('has a title', function() {
+    expect(browser.getTitle()).toEqual('Github user search');
   });
+
+  it('finds profiles', function() {
+    searchBox.sendKeys('spike01');
+    searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.get(0).getText()).toEqual('spike01');
+  });
+
+  it('finds the last Spike', function() {
+    searchBox.sendKeys('spike01');
+    searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.last().getText()).toEqual('spike01');
+  });
+
 });
